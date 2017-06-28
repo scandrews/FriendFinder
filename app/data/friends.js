2629df53b115bc5d3c6c835823b7d1342813f39a
Steve Andrews <scandrews2@gmail.com>
2017-06-28 08:19:56 -0400

    version as of Wed moring, displaying the pic doesn't work

---
+++ app/data/friends.js
@@ -0,0 +1,14 @@
+[{
+	"Name": "Julie",
+	"photo": "https://securepictures.match.com/photos/514/786/946dbec4-c505-e611-940e-a0d3c1f7be1e.jpeg?cr=2&h=200",
+	"scores": [
+	"2", "1", "4", "2", "5", "3", "3", "3"
+	]
+},
+{	"name": "Kathy",
+	"photo": "https://securepictures.match.com/photos/176/732/14df06e7-0659-e711-9417-a0d3c1f7be1f.jpeg?cr=2&h=200",
+	"scores": [
+	"4", "2", "2", "1", "4", "3", "2", "5"
+	]
+}
+]
---
+++ server.js
@@ -0,0 +1,79 @@
+//server for homework 13 - the matching page
+
+var express = require("express");
+var bodyParser = require ("body-parser");
+var path = require ("path");
+var app = express();
+var PORT = 3000;
+
+// set up express parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+var users = require("./app/data/friends.json");
+var matchtotal = 0;
+var user = {
+	name: "testname",
+	pic: "link to pic",
+	traits:[]
+};
+
+var bestMatch = {
+	name: "",
+	score: 0
+}
+
+for (i=0; i<users.length; i++){
+	console.log(users[i].name + users[i].photo + users[i].scores);
+}
+
+//  Routes
+//  Basic routes for pages
+app.get("/", function(req, res){
+	res.sendFile(path.join(__dirname, "/app/public/index.html"));
+});
+
+app.get("/matching", function(req, res){
+	console.log("got the matching page request");
+	res.sendFile(path.join(__dirname, "/app/public/matching.html"));
+});
+
+app.get("/style", function(req, res){
+	console.log("got the css request");
+	res.sendFile(path.join(__dirname, "/app/public/style.css"));
+});
+
+// routes for data
+
+app.post("/api/search", function(req, res){
+	console.log("got the search");
+	user = req.body;
+	console.log(user);
+	for (i=0; i<users.length; i++){
+		matchtotal = 0;
+		for (a=0; a<user.traits.length; a++){
+			matchtotal = matchtotal + (Math.abs(parseInt(user.traits[a]) - parseInt(users[i].scores[a])));
+			console.log(matchtotal);
+		};
+		if (bestMatch.score < matchtotal) {
+			bestMatch.score = matchtotal;
+			bestMatch.name = users[i].name;
+			bestMatch.pic = users[i].photo;
+		}
+	}
+	app.get("/matching.html/usermatch", function(req, res){
+		res.send(bestMatch);
+	});
+});
+
+app.get("/api/list", function(req, res){
+	console.log("got the request for the list");
+	res.sendFile(path.join(__dirname, "./app/data/friends.json"));
+})
+
+
+app.listen(PORT, function(){
+	console.log("listening on port " + PORT);
+});
